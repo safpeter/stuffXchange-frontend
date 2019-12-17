625982e4a5e2dcdf6f65149bab763065f953c750
Sáfár Péter <safpeter03@gmail.com>
2019-12-17 14:34:36 +0100

    logo is placed in the top right corner, icons with text are placed in the navigation-drawer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app id="app">
-     <v-col cols="15" sm="6" md="4" class="search-bar">
+     <v-col cols="15" sm="6" md="4" class="search-bar" >
       <v-text-field
+      class="orange--text"
         solo-inverted
         hide-details
         label="Search"
@@ -13,12 +14,22 @@
       permanent
       right
       floating
-      
+      text-color="orange"
       class="navbar"
       v-model="drawer"
       app
       clipped
-    >
+    >          <v-img class="logo" src="@/assets/StuffX-logo.png" height="34%" width="auto%"></v-img>
+   <v-list-item v-for="item in items" :key="item.text" text-color="orange">
+          <v-btn @click="gotoRoute(item.route)" class="orange--text" absolute color="transparent" depressed>     
+            <v-list-item-action>
+              <v-icon class="orange--text">{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content >
+              <v-list-item-title >{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-btn>
+        </v-list-item>
      </v-navigation-drawer>
       <v-content>
       <router-view/>
@@ -30,12 +41,21 @@ import home from "./views/Home";
 
 export default {
   name: "App",  
+  data: () => ({
+      items: [
+      { icon: "mdi-account", text: "My Profile" },
+      { icon: "mdi-cash-multiple", text: "Stuff to sell", route:"sell"},
+      { icon: "mdi-basket", text: "Stuff to Buy", route:"buy" },
+      { icon: "mdi-magnify-plus", text: "Advanced Search", route:"search" },
+      { icon: "mdi-logout", text: "Log out" }
+    ],
+  }), 
+
 }
 </script>
 <style scoped >
 .navbar {
   background-color:#424242 !important;
-  box-shadow:transparent;
 }
 
 #app {
@@ -44,6 +64,13 @@ export default {
 
 .search-bar{
   margin-left: 45%;
+  color: orange !important;
 }
+
+.logo {
+  margin-top: -1%;
+ 
+}
+
 
 </style>
